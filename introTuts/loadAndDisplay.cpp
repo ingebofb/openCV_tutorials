d57b55dc50168f9d7f3f2d47a899480e0e62bb6f
Ingeborg FB <ingeborg.brovoll@gmail.com>
2019-06-20 13:09:45 +0200

    Done tuts for image and video loading/creating and saving

--- introTuts/loadAndDisplay.cpp
+++ introTuts/loadAndDisplay.cpp
@@ -5,7 +5,6 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv){
-
 /*
 // Loading and displaying an image from file
 	Mat image = imread("/users/iflatinbrovoll/Documents/Bilder/whale.png");
@@ -20,18 +19,24 @@ int main(int argc, char** argv){
 
 	waitKey(0);
 	destroyWindow(windowName);
-
-
-// Creating a blank (light green in this case) image
+*/
+/*
+// Creating a blank (light green in this case) image and saving it to file
 	Mat blankImage(600, 800, CV_8UC3, Scalar(100, 250, 30));
 	String blankWindowName = "Blank Image";
 	namedWindow(blankWindowName);
 	imshow(blankWindowName, blankImage);
 
+	bool isSuccessBlank = imwrite("/users/iflatinbrovoll/Documents/Bilder/myImage.jpg", blankImage);
+	if (isSuccessBlank == false){
+		cout << "Failed to save the image" << endl;
+	}
+	cout << "Image is successfully saved to a file" << endl;
+
 	waitKey(0);
 	destroyWindow(blankWindowName);
-
-
+*/
+/*
 // Playing a video from file
 	VideoCapture cap("/users/iflatinbrovoll/Documents/Bilder/test.avi");
 	if (cap.isOpened() == false){
@@ -43,8 +48,8 @@ int main(int argc, char** argv){
 	cout << "Total number of frames in video: " << cap.get(CAP_PROP_FRAME_COUNT);
 	cin.get();
 
-	String windowName = "My first video";
-	namedWindow(windowName, WINDOW_NORMAL);
+	String windowNameVid = "My first video";
+	namedWindow(windowNameVid, WINDOW_NORMAL);
 
 	while (true){
 		Mat frame;
@@ -55,37 +60,49 @@ int main(int argc, char** argv){
 			break;
 		}
 
-		imshow(windowName, frame);
+		imshow(windowNameVid, frame);
 
 		if (waitKey(30) == 27){
 			cout << "Esc key is pressed by user. Stopping the video" << endl;
 			break;
 		}
 	}
-
 */
-// Playing a video from the default camera
 
-	VideoCapture cap(0);
-	if (cap.isOpened() == false){
+// Playing a video from the default camera and saving it to file
+
+	VideoCapture capCam(0);
+	if (capCam.isOpened() == false){
 		cout << "Could not open the video camera" << endl;
 	}
 
-	double dWidth = cap.get(CAP_PROP_FRAME_WIDTH);
-	double dHeight = cap.get(CAP_PROP_FRAME_HEIGHT);
-	cout << "Resolution of the video: " << dWidth << " x " << dHeight << endl;
+	int frameWidth = static_cast<int>(capCam.get(CAP_PROP_FRAME_WIDTH));
+	int frameHeight = static_cast<int>(capCam.get(CAP_PROP_FRAME_HEIGHT));
+
+	Size frameSize(frameWidth, frameHeight);
+	int fps = 10;
+
+	// M, J, P, G --> Codec for motion JPEG
+	VideoWriter oVideoWriter("/users/iflatinbrovoll/Documents/Bilder/myVideo.avi", VideoWriter::fourcc('M', 'J', 'P', 'G'),
+		fps, frameSize);
+
+	if (oVideoWriter.isOpened() == false){
+		cout << "Could not save the video to a file" << endl;
+	}
 
 	String windowNameCam = "My camera feed";
-	namedWindow(windowNameCam, WINDOW_NORMAL);
+	namedWindow(windowNameCam);
 
 	while (true){
 		Mat frameCam;
-		bool bSuccessCam = cap.read(frameCam); // Read frame from video
+		bool bSuccessCam = capCam.read(frameCam); // Read frame from video
 
 		if (bSuccessCam == false){ // Breaks while loop if end of video
 			cout << "Found the end of the video" << endl;
 			break;
 		}
+
+		oVideoWriter.write(frameCam);
 
 		imshow(windowNameCam, frameCam);
 
